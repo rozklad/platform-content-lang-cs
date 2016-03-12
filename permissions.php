dc4b89363a82f7c50ca563079082682c9242a014
rozklad <jan.rozklad@gmail.com>
2016-03-12 19:26:05 +0100

    Default language

---
+++ permissions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Part of the Platform Content extension.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the WTFPL license.
+ *
+ * @package    Platform Content extension
+ * @version    3.0.0
+ * @author     Sanatorium
+ * @license    WTFPL
+ * @copyright  2016, Sanatorium
+ * @link       http://sanatorium.ninja
+ */
+
+return [
+
+	'index'  => 'Vypsat obsah',
+	'create' => 'Vytvořit obsah',
+	'copy'   => 'Kopírovat obsah',
+	'edit'   => 'Upravit obsah',
+	'delete' => 'Smazat obsah',
+
+];
